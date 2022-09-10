49bd498c82a5eceea84b69a147e7127614b503bf
balajidharma <balajidharma@hotmail.com>
2022-09-10 09:19:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-mwvh98
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-mwvh98)
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Layout>
+      <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </Layout>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Layout from './components/Layout/Layout.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Layout,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
